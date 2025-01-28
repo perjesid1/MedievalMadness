5e3aab16ae5773b9bad993fd281c525ef5675150
Perjési Dániel <perjesid1@gmail.com>
2025-01-28 21:26:35 +0100

    Added constants instead of "magic values". Added comments.

--- Global.gd
+++ Global.gd
@@ -1,12 +1,24 @@
 extends Node
+## Autoload Singleton for handling global functions and variables.
 
+
+## Path to the settings file.
+const SETTINGS_PATH: String = "user://settings.cfg"
+
+
+## Contains the settings for the game.
+## Each setting group is Dictionary, where the keys are the settings' names.
 var settings: Dictionary = {}
+## True when the game loads for the first time, then set to false.
+## Used to determine the starting point for the theme song of the main menu.
+var first_load: bool = true
 
 
 func _ready() -> void:
 	load_settings()
 
 
+## Saves the contents of the "settings" Dictionary to the settings file to the path defined in the SETTINGS_PATH constant.
 func save_settings() -> void:
 	var config: ConfigFile = ConfigFile.new()
 	var current_section: Dictionary
@@ -18,14 +30,15 @@ func save_settings() -> void:
 		else:
 			print("Failed to save settings! Invalid section type.")
 	
-	var error: Error = config.save("user://settings.cfg")
+	var error: Error = config.save(SETTINGS_PATH)
 	if error != OK:
 		print("Failed to save settings! Unable to save file.")
 
-
+## Loads the settings file from the path defined in the SETTINGS_PATH constant.
+## The settings will be accessible through this classes "settings" Dictionary.
 func load_settings() -> void:
 	var config: ConfigFile = ConfigFile.new()
-	var error: Error = config.load("user://settings.cfg")
+	var error: Error = config.load(SETTINGS_PATH)
 	if error != OK:
 		print("Failed to load settings!")
 	else:
