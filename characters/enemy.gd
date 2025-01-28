a09b47d6624793abb4d07abe8211c49b9a47f1fb
Perjési Dániel <perjesid1@gmail.com>
2025-01-28 21:37:46 +0100

    Made variables private. Added comments.

--- characters/enemy.gd
+++ characters/enemy.gd
@@ -1,16 +1,23 @@
 class_name Enemy
 extends CharacterBody2D
+## Base class for all enemy characters.
 
 
+## Maximum health of the enemies.
 const MAX_HP: int = 100
+## Terminal velocity of the cannon balls.
 const TERMINAL_VELOCITY: float = 1000.0
 
+
+## Health of the foe.
 @export var health: int
 
-@onready var animated_sprite_2d: AnimatedSprite2D
+
+@onready var _animated_sprite_2d: AnimatedSprite2D
 
 
 func _ready() -> void:
+	# Play default animation when entering the world.
 	if has_method("play_animation"):
 		call("play_animation", "run")
 
@@ -25,6 +32,8 @@ func _physics_process(_delta: float) -> void:
 			handle_collision(impact_velocity)
 
 
+## Calculates the damage done based on the length of the impact velocity vector.
+## This is done to ensure the power of the shot has an effect on the damage done.
 func handle_collision(impact_velocity: float) -> void:
 	var damage_ratio: float = clampf(impact_velocity / TERMINAL_VELOCITY, 0.0, 1.0)
 	var damage: int = ceil(MAX_HP * damage_ratio)
@@ -35,14 +44,19 @@ func take_damage(damage: int) -> void:
 	health -= damage
 	if health < 0:
 		die()
-	elif animated_sprite_2d != null:
-		animated_sprite_2d.play(&"hurt")
-		await animated_sprite_2d.animation_finished
-		animated_sprite_2d.play(&"idle")
+	elif _animated_sprite_2d != null:
+		_animated_sprite_2d.play(&"hurt")
+		await _animated_sprite_2d.animation_finished
+		_animated_sprite_2d.play(&"idle")
 
 
 func die() -> void:
-	if animated_sprite_2d != null:
-		animated_sprite_2d.play(&"death")
-		await animated_sprite_2d.animation_finished
+	if _animated_sprite_2d != null:
+		_animated_sprite_2d.play(&"death")
+		await _animated_sprite_2d.animation_finished # We should not remove the foe until the animation is finished.
 	queue_free()
+
+
+func heal(health_to_add: int) -> void:
+	# Make sure we cannot overheal enemies.
+	health += clampi(health_to_add, 0, MAX_HP)
